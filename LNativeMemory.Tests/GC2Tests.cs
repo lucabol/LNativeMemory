4bb2ee155f4f982cc94dc1b70f8603a156583697
Luca Bolognese <lucabol@microsoft.com>
2018-12-18 15:06:11 +0100

    ADD: GC Stopping Functions

---
+++ LNativeMemory.Tests/GC2Tests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace LNativeMemory.Tests {
+
+    // XUnit executes all tests in a class sequentially, so nothing to do here
+    public class GC2Tests {
+
+        [Fact]
+        public void NoAllocationToLimit() {
+            try {
+                var succeeded = GC2.TryStartNoGCRegion(100, () => Assert.True(false, "No GC should have been triggered"));
+                Assert.True(succeeded);
+
+                var bytes = new Byte[100];
+            } finally {
+                GC2.EndNoGCRegion();
+            }
+        }
+
+        [Fact]
+        public void AllocatingOverLimitTriggersTheAction() {
+            try {
+                var triggered = false;
+                var succeeded = GC2.TryStartNoGCRegion(100, () => triggered = true);
+                Assert.True(succeeded);
+
+                var bytes = new Byte[1001];
+                Thread.Sleep(200);
+                Assert.True(triggered);
+            } finally {
+                GC2.EndNoGCRegion();
+            }
+        }
+
+        [Fact]
+        public void CanCallMultipleTimes() {
+
+            for (int i = 0; i < 3; i++) {
+                try {
+                    var triggered = false;
+                    var succeeded = GC2.TryStartNoGCRegion(100, () => triggered = true);
+                    Assert.True(succeeded);
+
+                    var bytes = new Byte[1001];
+                    Thread.Sleep(200);
+                    Assert.True(triggered);
+                } finally {
+                    GC2.EndNoGCRegion();
+                }
+            }
+        }
+    }
+}
---
+++ LNativeMemory/GC2.cs
@@ -0,0 +1,43 @@
+﻿namespace LNativeMemory {
+    using System;
+    using System.Diagnostics.Tracing;
+
+    public static class GC2 {
+        static private GcEventListener _action;
+
+        public static bool TryStartNoGCRegion(long totalSize, Action actionWhenAllocatedMore) {
+
+            var succeeded = GC.TryStartNoGCRegion(totalSize);
+            _action = new GcEventListener(actionWhenAllocatedMore);
+            return succeeded;
+        }
+
+        public static void EndNoGCRegion() {
+            try {
+                _action = null;
+                System.GC.EndNoGCRegion();
+            } catch (InvalidOperationException) {
+            }
+
+        }
+    }
+
+    internal sealed class GcEventListener : EventListener {
+        Action _action;
+
+        internal GcEventListener(Action action) {
+            _action = action;
+        }
+        protected override void OnEventSourceCreated(EventSource eventSource) {
+            if (eventSource.Name.Equals("Microsoft-Windows-DotNETRuntime")) {
+                EnableEvents(eventSource, EventLevel.Verbose, (EventKeywords)(-1));
+            }
+        }
+
+        protected override void OnEventWritten(EventWrittenEventArgs eventData) {
+            if (eventData.EventName.StartsWith("GCStart")) {
+                _action();
+            }
+        }
+    }
+}
