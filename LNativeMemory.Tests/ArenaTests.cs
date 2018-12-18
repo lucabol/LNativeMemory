db52adf54b67e51af1dc9fefc99dbe627508127c
Luca Bolognese <lucabol@microsoft.com>
2018-12-18 14:05:34 +0100

    First commit

---
+++ LNativeMemory.Tests/ArenaTests.cs
@@ -0,0 +1,87 @@
+using System;
+using Xunit;
+using LNativeMemory;
+using System.Runtime.InteropServices;
+
+namespace ArenaTests {
+    [StructLayout(LayoutKind.Auto)]
+    struct CStruct {
+        public int X;
+        public float Y;
+        bool b;
+        double d;
+        Decimal dec;
+    }
+    public class Tests {
+
+        [Fact]
+        public void CanAllocateStruct() {
+            using (var ar = new Arena(1_000_000)) {
+                ref var s = ref ar.FastAlloc<CStruct>();
+                Assert.Equal(0, s.X);
+                s.X = 3;
+                Assert.Equal(3, s.X);
+
+                var n = 100;
+                var span = ar.FastAlloc<CStruct>(n);
+                foreach (var c in span) Assert.Equal(0, c.X);
+                for (int i = 0; i < n; i++) span[i].X = 3;
+                foreach (var c in span) Assert.Equal(3, c.X);
+            }
+        }
+
+        [Fact]
+        public void CanAllocatePrimitiveTypes() {
+            using (var ar = new Arena(1_000)) {
+                var ispan = ar.Alloc<int>(10);
+                var fspan = ar.Alloc<float>(10);
+                var dspan = ar.Alloc<double>(10);
+                var bspan = ar.Alloc<bool>(10);
+                var despan = ar.Alloc<Decimal>(10);
+
+                for (int i = 0; i < 10; i++) {
+                    Assert.Equal(0, ispan[i]);
+                    Assert.Equal(0.0, fspan[i]);
+                    Assert.Equal(0.0, dspan[i]);
+                    Assert.Equal(0, despan[i]);
+                    Assert.False(bspan[i]);
+
+                    ispan[i] = 3;
+                    fspan[i] = 3.0f;
+                    dspan[i] = 3.0;
+                    bspan[i] = true;
+                    despan[i] = 3;
+
+                    Assert.Equal(3, ispan[i]);
+                    Assert.Equal(3.0, fspan[i]);
+                    Assert.Equal(3.0, dspan[i]);
+                    Assert.Equal(3, despan[i]);
+                    Assert.True(bspan[i]);
+                }
+
+            }
+        }
+
+        [Fact]
+        public void ThrowsExceptionWhenMemoryIsFull() {
+            using (var ar = new Arena(100)) {
+                var i = 0;
+                Assert.Throws<OutOfMemoryException>(() => {
+                    while (i < 1_000_000) { ar.Alloc<byte>(); i++; }
+                    return 0;
+                });
+                Assert.Equal(100, i);
+            }
+        }
+
+        [Fact]
+        public void GeCorrectRemainingSize() {
+            using (var ar = new Arena(1000)) {
+                ar.FastAlloc<float>();
+                Assert.Equal(1000 - sizeof(float), ar.BytesLeft);
+                ar.FastAlloc<decimal>(10);
+                Assert.Equal(1000 - sizeof(float) - sizeof(decimal) * 10, ar.BytesLeft);
+            }
+        }
+    }
+}
---
+++ LNativeMemory/Arena.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace LNativeMemory {
+
+    // It is a mutable value type implementing an interface, all hell break loose!
+    // But C# doesn't box it when in an using statement, otherwise it is supposed to be passed around by ref
+    public unsafe struct Arena : IDisposable {
+        private byte* _start;
+        private byte* _nextAlloc;
+        private int _size;
+
+        public Arena(int totalBytes) {
+            Debug.Assert(totalBytes > 0);
+
+            _start = _nextAlloc = (byte*)Marshal.AllocHGlobal(totalBytes).ToPointer();
+            _size = totalBytes;
+            for (int i = 0; i < totalBytes; ++i) _start[i] = 0;
+        }
+
+        public ref T Alloc<T>() where T : unmanaged {
+            var sizeType = sizeof(T);
+            if (_nextAlloc + sizeType > _start + _size) throw new OutOfMemoryException
+                    ($"Trying to allocate {sizeType} bytes for a type {typeof(T).FullName}.\nStart: {(int)_start}\nNextAlloc: {(int)_nextAlloc}\nSize:{(int)_size}");
+
+            var ptr = _nextAlloc;
+            _nextAlloc += sizeType;
+            return ref (*((T*)ptr));
+        }
+
+        public Span<T> Alloc<T>(int n) where T : unmanaged {
+            var sizeArray = sizeof(T) * n;
+            if (_nextAlloc + sizeArray > _start + _size) throw new OutOfMemoryException
+                    ($"Trying to allocate {sizeArray} bytes for an array {typeof(T).FullName}[{n}].\nStart: {(int)_start}\nNextAlloc: {(int)_nextAlloc}\nSize:{_size}");
+
+            var ptr = _nextAlloc;
+            _nextAlloc += sizeArray;
+            return new Span<T>(ptr, n);
+        }
+
+        public ref T FastAlloc<T>() where T : unmanaged {
+            var sizeType = sizeof(T);
+            Debug.Assert(_nextAlloc + sizeType <= _start + _size,
+                    $"Trying to allocate {sizeType} bytes for a type {typeof(T).FullName}.\nStart: {(int)_start}\nNextAlloc: {(int)_nextAlloc}\nSize:{(int)_size}");
+
+            var ptr = _nextAlloc;
+            _nextAlloc += sizeType;
+            return ref (*((T*)ptr));
+        }
+
+        public Span<T> FastAlloc<T>(int n) where T : unmanaged {
+            var sizeArray = sizeof(T) * n;
+            Debug.Assert(_nextAlloc + sizeArray <= _start + _size,
+                    $"Trying to allocate {sizeArray} bytes for an array {typeof(T).FullName}[{n}].\nStart: {(int)_start}\nNextAlloc: {(int)_nextAlloc}\nSize:{_size}");
+
+            var ptr = _nextAlloc;
+            _nextAlloc += sizeArray;
+            return new Span<T>(ptr, n);
+        }
+
+        public int BytesLeft => _size - (int)(_nextAlloc - _start);
+        public int TotalBytes => _size;
+
+        public void Dispose() {
+            Marshal.FreeHGlobal((IntPtr)_start);
+        }
+    }
+
+}
